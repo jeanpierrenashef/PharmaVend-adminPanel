37c0045ba6d41b81921e320f8fb068221ada403f
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-29 23:14:44 +0300

    Initialized the orders page as well as some dependencies

---
+++ src/pages/Orders.jsx
@@ -0,0 +1,7 @@
+import react from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+const Orders = () => {
+    const navigate = useNavigate();
+}
