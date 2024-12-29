0154113306f198e78344ec81911606932619e599
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-29 22:43:56 +0300

    Added the laod user reducer in the user slice

---
+++ src/redux/users/slice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+    name: "users",
+    initialState: {
+        list:[],
+    },
+    reducers:{
+        loadUsers: (state, action) => {
+            const users = action.payload;
+
+            return {
+                ...state,
+                list:users,
+            };
+        },
+    }
+})
