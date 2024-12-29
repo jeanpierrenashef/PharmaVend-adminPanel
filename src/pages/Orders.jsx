24cac7e3721cd4af6bc4d270cb8ea4ee66c5d11d
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-29 23:49:35 +0300

    Added the get users in the orders page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,23 @@
-import react from "react";
+import react, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
 
 const Orders = () => {
-    //const navigate = useNavigate();
+    const navigate = useNavigate();
+    const transactions = useSelector((global)=>global.transactions);
+    const users = useSelector((global)=>gloval.transactions);
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        axios.get("http://127.0.0.1:8000/api/admin/users"
+        ).then(({data})=>{
+            const action = {type: "users/loadUsers", payload: data}
+            dispatch(action)
+        });
+    },[])
+
+
     return(
         <h1>hello</h1>
     );
