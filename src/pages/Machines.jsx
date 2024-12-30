571d46c95b497c81e9c577eb5f462d59c9312eb8
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-31 00:21:49 +0300

    Initial setup for the machines page

--- src/pages/Machines.jsx
+++ src/pages/Machines.jsx
@@ -6,7 +6,37 @@ const Machines = () => {
     const dispatch = useDispatch();
     const machines = useSelector((global)=>global.machines.list);
 
-    useEffect(()=>{
-        axios.get("")
-    })
+    useEffect(() => {
+        axios.get("http://127.0.0.1:8000/api/admin/machines").then(({ data }) => {
+            const action = { type: "machines/loadmachines", payload: data };
+            dispatch(action);
+        });
+    }, []);
+
+    return(
+        <div className="machines-page">
+            <Navbar />
+            <div className="main-content">
+                <h1>Machines</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Location</th>
+                            <th>Latitude</th>
+                            <th>Longitude</th>
+                            <th>Status</th>
+                            <th>Created At</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <MachineRow />
+                    </tbody>
+                </table>
+
+                
+            </div>
+
+        </div>
+    )
 }
