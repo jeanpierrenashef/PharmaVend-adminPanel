4d7936551550d678ff48d6bafd9485e677b9b62f
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2025-01-02 00:17:19 +0300

    Added the option to traverse between machines

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -5,6 +5,12 @@ import { useSelector, useDispatch } from "react-redux";
 
 const Inventory = () => {
     const dispatch = useDispatch();
+    const inventory = useSelector((global) => global.inventory.list);
+    const machines = useSelector((state) => state.machines.list);
+    const products = useSelector((state) => state.products.list);
+
+    const [currentMachineIndex, setCurrentMachineIndex] = useState(0);
+
     
     useEffect(()=>{
         axios.get("http://127.0.0.1:8000/api/admin/inventory").then(({data}) => {
@@ -13,9 +19,23 @@ const Inventory = () => {
         })
     }, []);
 
+    const handlePrevMachine = () => {
+        setCurrentMachineIndex((prev) => (prev > 0 ? prev - 1 : machines.length - 1));
+    };
+
+    const handleNextMachine = () => {
+        setCurrentMachineIndex((prev) => (prev < machines.length - 1 ? prev + 1 : 0));
+    };
+
+    const currentMachine = machines[currentMachineIndex];
+
+    const currentMachineInventory = inventories.filter(
+        (inventory) => inventory.machine_id === currentMachine?.id
+    );
+
     return(
         <div>
-            
+
         </div>
 
     );
