aa5ef47754a5d78cd1e04395bedae023ba877305
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2025-01-01 23:39:55 +0300

    Initialized the inventory file

---
+++ src/pages/Inventory.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {loadInventories} from "../redux/inventories/slice.js"
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+const Inventory = () => {
+
+}
+export default Inventory;
