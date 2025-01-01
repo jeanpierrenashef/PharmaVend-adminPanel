ec1fd8018a39cb64208a6560058c9962aa7bcc2e
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2025-01-01 18:28:50 +0300

    Fixed a misspelled error

--- src/components/CustomerRow.jsx
+++ src/components/CustomerRow.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const CustomerRow = ({ customer }) => {
     return (
         <tr>
-            <td>{customer.id}</td>
-            <td>{customer.name}</td>
+            <td>#00{customer.id}</td>
+            <td>{customer.username}</td>
             <td>{customer.email}</td>
             <td>{customer.totalOrders}</td>
             <td>{new Date(customer.created_at).toISOString().split("T")[0]}</td>
