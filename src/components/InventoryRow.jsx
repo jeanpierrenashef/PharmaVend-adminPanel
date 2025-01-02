dc425c360c8f66c70260702d4a5c6d4d216f6e49
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2025-01-03 01:16:57 +0300

    Design enhancements in the inventory component

--- src/components/InventoryRow.jsx
+++ src/components/InventoryRow.jsx
@@ -6,21 +6,23 @@ const InventoryRow = ({ product, quantity, onUpdateQuantity }) => {
             <td>{product?.id }</td>
             <td>{product?.name || "Unknown"}</td>
             <td>
-                <button
-                    className="action-button subtract-button"
-                    onClick={() => onUpdateQuantity(product.id, -1)}
-                    title="Decrease Quantity"
-                >
-                    <i className="mdi mdi-minus-circle-outline"></i>
-                </button>
-                {quantity}
-                <button
-                    className="action-button add-button"
-                    onClick={() => onUpdateQuantity(product.id, 1)}
-                    title="Increase Quantity"
-                >
-                    <i className="mdi mdi-plus-circle-outline"></i>
-                </button>
+                <div className="quantity-controls">
+                    <button
+                        className="action-button subtract-button"
+                        onClick={() => onUpdateQuantity(product.id, -1)}
+                        title="Decrease Quantity"
+                    >
+                        <i className="mdi mdi-minus-circle-outline"></i>
+                    </button>
+                    <span className="quantity-value">{quantity}</span>
+                    <button
+                        className="action-button add-button"
+                        onClick={() => onUpdateQuantity(product.id, 1)}
+                        title="Increase Quantity"
+                    >
+                        <i className="mdi mdi-plus-circle-outline"></i>
+                    </button>
+                </div>
             </td>
         </tr>
     );
